b73a658483343088f1e78e9b77df4dd5b7bf71db
Robbe Gaeremynck <robbe.gaeremynck@outlook.com>
2025-10-28 12:09:38 +0100

    Change openwifi overlay and openwifi board overlay placeholder names in boot.cmd

--- kernel/openwifi/files/boot.cmd
+++ kernel/openwifi/files/boot.cmd
@@ -1,5 +1,5 @@
 # Program openwifi bitstream
-setenv bit_file openwifi_bitstream.bit
+setenv bit_file bitstream.bit
 setenv bit_loadaddr 0x03000000
 setenv program_openwifi_bitstream "load ${devtype} ${bootpart} ${bit_loadaddr} ${bit_file};fpga loadb 0 ${bit_loadaddr} ${filesize}"
 run program_openwifi_bitstream
@@ -10,10 +10,10 @@ bootm start 0x04000000
 
 # Tweaking device tree
 fdt addr ${fdtaddr} # Redundant, should be already done by bootm start
-load ${devtype} ${bootpart} 0x1000000 ${openwifi_overlay_filename}
+load ${devtype} ${bootpart} 0x1000000 [openwifi_overlay_filename]
 fdt resize ${filesize}
 fdt apply 0x1000000
-load ${devtype} ${bootpart} 0x2000000 ${openwifi_board_overlay_filename}
+load ${devtype} ${bootpart} 0x2000000 [openwifi_board_overlay_filename]
 fdt resize ${filesize}
 fdt apply 0x2000000
 
