9ce9f9d27961c24de46e5f610e5698e34d595ba4
Robbe Gaeremynck <robbe.gaeremynck@outlook.com>
2025-10-28 15:50:56 +0100

    Update default and openwifi boot.cmd scripts

--- kernel/openwifi/files/boot.cmd
+++ kernel/openwifi/files/boot.cmd
@@ -1,21 +1,28 @@
 # Program openwifi bitstream
 setenv bit_file bitstream.bit
 setenv bit_loadaddr 0x03000000
-setenv program_openwifi_bitstream "load ${devtype} ${bootpart} ${bit_loadaddr} ${bit_file};fpga loadb 0 ${bit_loadaddr} ${filesize}"
+setenv program_openwifi_bitstream "load ${devtype} ${bootpart} ${bit_loadaddr} ${bit_file};fpga loadb 0 ${bit_loadaddr} ${filesize};"
 run program_openwifi_bitstream
 
 # Extract default device_tree
 load ${devtype} ${bootpart} 0x04000000 fit.itb
 bootm start 0x04000000
 
+setenv fdt_copy_addr 0x03000000
+
 # Tweaking device tree
-fdt addr ${fdtaddr} # Redundant, should be already done by bootm start
-load ${devtype} ${bootpart} 0x1000000 [openwifi_overlay_filename]
+cp.b ${fdtaddr} ${fdt_copy_addr} ${filesize}
+fdt addr ${fdt_copy_addr}
+
+# Apply common openwifi hardware
+load ${devtype} ${bootpart} 0x1000000 openwifi_overlay_filename
 fdt resize ${filesize}
 fdt apply 0x1000000
-load ${devtype} ${bootpart} 0x2000000 [openwifi_board_overlay_filename]
+
+# Apply openwifi board specific hardware
+load ${devtype} ${bootpart} 0x2000000 openwifi_board_overlay_filename
 fdt resize ${filesize}
 fdt apply 0x2000000
 
 # Start kernel with tweaked device tree
-bootm 0x04000000 - ${fdtaddr}
+bootm 0x04000000 - ${fdt_copy_addr}
